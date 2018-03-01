67586759114db4da4cd5e53e2afba5581dec46e1
Taras Hrynchuk <Taras_Hrynchuk@epam.com>
2018-03-01 15:58:55 +0200

    project structure

---
+++ src/main/java/com/tramonti/weather/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.tramonti.weather.controller;
+
+import com.tramonti.weather.domain.User;
+import com.tramonti.weather.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * Created by tarashrynchuk on 3/1/18.
+ */
+@RestController("users")
+public class UserController {
+	@Autowired
+	private UserService userService;
+
+	@GetMapping
+	public List<User> getAllUsers() {
+		return  userService.getUsers();
+	}
+
+	//TODO: crud users operations
+
+	//TODO: get user by  username and
+
+	//TODO: use loggers
+
+	//@ExceptionHandler - map exception to http error
+	//@ControllerAdvice
+
+
+}
---
+++ src/main/java/com/tramonti/weather/service/UserService.java
@@ -0,0 +1,13 @@
+package com.tramonti.weather.service;
+
+import com.tramonti.weather.domain.User;
+
+import java.util.List;
+
+/**
+ * Created by tarashrynchuk on 3/1/18.
+ */
+
+public interface UserService {
+	List<User> getUsers();
+}
