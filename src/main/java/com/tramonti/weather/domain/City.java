200e22347f749f06bbdbdadd1bda66bf3017771a
Olesia Serban <OlesiaSerban@gmail.com>
2018-03-06 19:34:54 +0200

    added Guava, City modified with Guava toString, hashCode and equals

--- src/main/java/com/tramonti/weather/domain/City.java
+++ src/main/java/com/tramonti/weather/domain/City.java
@@ -1,27 +1,55 @@
 
 package com.tramonti.weather.domain;
 
+import com.google.common.base.MoreObjects;
+import com.google.common.base.Objects;
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 //TODO: refactor equals, hash, toString using Java8 Objects
 //or Guava or Apache Commons
-
-@Data
+@Getter
+@Setter
 public class City {
 
     @SerializedName("id")
     @Expose
-    public Integer id;
+    private Integer id;
     @SerializedName("name")
     @Expose
-    public String name;
+    private String name;
     @SerializedName("coord")
     @Expose
-    public Coordinates coordinates;
+    private Coordinates coordinates;
     @SerializedName("country")
     @Expose
-    public String country;
+    private String country;
 
+    @Override
+    public String toString() {
+        return MoreObjects.toStringHelper(this)
+                .add("id", id)
+                .add("name", name)
+                .add("coordinates", coordinates)
+                .add("country", country)
+                .toString();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this.id, this.name, this.country, this.coordinates);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        final City other = (City) obj;
+        return Objects.equal(this.id, other.id)
+                && Objects.equal(this.name, other.name)
+                && Objects.equal(this.country, other.country)
+                && Objects.equal(this.coordinates, other.coordinates);
+    }
 }
